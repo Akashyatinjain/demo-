da0f8a9c995290a44c4d4c92a11c2e0f1818221e
Akashyatinjain <aj0881871@gmail.com>
2025-03-23 12:52:24 +0530

    Add files via upload

---
+++ tutorials/script.js
@@ -0,0 +1,4 @@
+document.getElementById("leftMenuToggle").addEventListener("click", function() {
+  var leftMenu = document.getElementById("leftsidebox");
+  leftMenu.classList.toggle("active");
+});
