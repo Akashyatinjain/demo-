055de507253af2da7c65c05b21defc0c2c46a7b3
Akashyatinjain <aj0881871@gmail.com>
2025-03-23 23:46:52 +0530

    Delete article directory

--- article/script.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("leftMenuToggle").addEventListener("click", function() {
-  var leftMenu = document.getElementById("leftsidebox");
-  leftMenu.classList.toggle("active");
-});
-
-document.getElementById("menuToggle").addEventListener("click", function() {
-  document.body.classList.toggle("menu-active");
-});
