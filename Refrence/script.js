7e1f8505c03301afd32504a1a037f65bbfc8f8ec
Akashyatinjain <aj0881871@gmail.com>
2025-03-27 00:36:57 +0530

    Delete Refrence directory

--- Refrence/script.js
+++
@@ -1,4 +0,0 @@
-document.getElementById("leftMenuToggle").addEventListener("click", function() {
-    var leftMenu = document.getElementById("leftsidebox");
-    leftMenu.classList.toggle("active");
-  });
